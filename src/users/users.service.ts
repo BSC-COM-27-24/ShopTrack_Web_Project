7919d62dcee1ce143b89c1b1efd59422e97e1e5b
Bsc-inf-02-24 <bsc-inf-02-24@unima.ac.mw>
2026-04-19 14:05:56 +0200

    added assignment assertions to all uninitialized properties in user.entities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,39 +1,24 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-<<<<<<< HEAD
-import * as bcrypt from 'bcryptjs';   // ← Use * as bcrypt (not default import)
-import { CreateUserDto } from './dto/create-user.dto';
-
-export interface User {
-    id: number;
-    name: string;
-    username: string;
-    password: string;
-    email: string;
-    role: 'Admin' | 'Attendant';
-}
-
-@Injectable()
-export class UsersService {
-    deleteUser(id: number) {
-        throw new Error('Method not implemented.');
-    }
-    updateUser: any;
-    findById(id: number) {
-        throw new Error('Method not implemented.');
-    }
-    findAll() {
-        throw new Error('Method not implemented.');
-    }
-    private users: User[] = [];
-=======
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcryptjs'; // ← Use * as bcrypt (not default import)
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
->>>>>>> 85fbd7a0acf1c70ea0ac1ba5499f2336688f21a3
+  deleteUser(id: number) {
+      throw new Error('Method not implemented.');
+  }
+  updateUser(id: number, updateUserDto: UpdateUserDto) {
+      throw new Error('Method not implemented.');
+  }
+  findById(id: number) {
+      throw new Error('Method not implemented.');
+  }
+  findAll() {
+      throw new Error('Method not implemented.');
+  }
 
   constructor(
     @InjectRepository(User)
