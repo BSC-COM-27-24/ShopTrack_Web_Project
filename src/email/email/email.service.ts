8df330ab2b843fb276c78b69c83fb1629e83343a
BSC-COM-27-24 <bsc-com-27-24@unima.ac.mw>
2026-04-19 13:40:38 +0200

    added some of the functionality to enable email sending

---
+++ src/email/email/email.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+
+@Injectable()
+export class EmailService {
+
+    //THE EMAIL TRANSPORTER DEFINITION
+
+    private transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    //THE MESSAGGE ITSELF
+    async sendEmail(to: string, subject: string, text: string) {
+        try {
+            const info = await this.transporter.sendMail({
+                from: `"ShopTrack" <${process.env.EMAIL_USER}>`,
+                to: to,
+                subject: subject,
+                text: text,
+            });
+
+            console.log('Email sent:', info.response);
+            return info;
+        } catch (error) {
+            console.error('Error sending email:', error);
+            throw error;
+        }
+    }
+}
