80c03c831f7a64ee209350103a24eec74c9d1c23
BSC-COM-27-24 <bsc-com-27-24@unima.ac.mw>
2026-04-20 07:32:57 +0200

    Adjusted create user dto

--- src/auth/dtos/create-admin.dto.ts
+++ src/auth/dtos/create-admin.dto.ts
@@ -24,11 +24,5 @@ export class createAdminDto {
     @MinLength(8, { message: 'Password must be at least 8 characters long' })
     password: string;
 
-    //OPTIONAL
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(8)
-    confirmPassword: string;
-
 
 }
